31b339f708f66f5aa48b57688d164b5b0a34daf1
Adithya Kumar <adithya@composewell.com>
2020-12-17 02:09:58 +0530

    Refactor tests and add tests for decompress (resize)
    - Replace StreamD tests with SerialT tests
    - Rename tests
    - Read and write with different buffer sizes
    - Add "decompress . compress i"

--- test/Main.hs
+++ test/Main.hs
@@ -1,5 +1,6 @@
 module Main (main) where
 
+import Control.Monad.IO.Class (MonadIO(..))
 import Data.Word (Word8)
 import Data.Function ((&))
 import System.IO (IOMode(..), openFile, hClose)
@@ -11,7 +12,6 @@ import Test.QuickCheck.Monadic (monadicIO)
 
 import qualified Streamly.Internal.Data.Array.Storable.Foreign.Types as A
 import qualified Streamly.Internal.Data.Stream.IsStream as S
-import qualified Streamly.Internal.Data.Stream.StreamD as D
 import qualified Streamly.Internal.FileSystem.Handle as H
 import qualified Streamly.Internal.Memory.ArrayStream as AS
 
@@ -23,29 +23,68 @@ cantrbry_alice29_txt base = base ++ "cantrbry/alice29.txt"
 genArrayW8List :: Gen [A.Array Word8]
 genArrayW8List = listOf $ A.fromList <$> listOf chooseAny
 
-identitySimple :: Property
-identitySimple =
-    forAll ((,) <$> genArrayW8List <*> choose (-10, 10))
+decompressResizedcompressSimple :: Property
+decompressResizedcompressSimple =
+    forAll ((,) <$> genArrayW8List <*> choose (-1, 10))
         $ \(lst, i) ->
-              let strm = D.fromList lst
+              let strm = S.fromList lst
                in monadicIO
                       $ do
-                          lst1 <-
-                              D.toList $ decompressResizedD $ compressD i strm
+                          lst1 <- S.toList $ decompressResized $ compress i strm
                           return $ lst == lst1
 
-identityRealWorld :: Int -> String -> IO ()
-identityRealWorld i f = do
-    let f1Strm =
-            S.bracket_ (openFile f ReadMode) hClose
-                $ \h ->
-                      S.unfold H.readChunks h & compress i & decompressResized
-                          & AS.concat
-    f1 <- S.toList f1Strm
-    f2 <-
-        S.toList
-            $ S.bracket_ (openFile f ReadMode) hClose $ \h -> S.unfold H.read h
-    f1 `shouldBe` f2
+decompressResizedcompress :: String -> Property
+decompressResizedcompress f =
+    forAll (choose (-1, 10))
+        $ \i ->
+              monadicIO
+                  $ liftIO
+                  $ do
+                      let f1Strm =
+                              S.bracket_ (openFile f ReadMode) hClose
+                                  $ \h ->
+                                        S.unfold
+                                            H.readChunksWithBufferOf (300, h)
+                                                & compress i
+                                                & decompressResized
+                                                & AS.concat
+                      f1 <- S.toList f1Strm
+                      f2 <-
+                          S.toList
+                              $ S.bracket_ (openFile f ReadMode) hClose
+                              $ \h -> S.unfold H.read h
+                      f1 `shouldBe` f2
+
+decompressCompress :: String -> Property
+decompressCompress f =
+    forAll (choose (-1, 10))
+        $ \i ->
+              monadicIO
+                  $ liftIO
+                  $ do
+                      let tmp = "/tmp/test.lz4"
+                          openers = do
+                              r <- openFile f ReadMode
+                              w <- openFile tmp WriteMode
+                              return (r, w)
+                          closers (r, w) = hClose r >> hClose w
+                      (r, w) <- openers
+                      S.unfold H.readChunksWithBufferOf (300, r)
+                          & compress i
+                          & H.fromChunks w
+                      closers (r, w)
+                      f1 <-
+                          S.toList
+                              $ S.bracket_ (openFile tmp ReadMode) hClose
+                              $ \h ->
+                                    S.unfold H.readChunksWithBufferOf (50, h)
+                                        & decompress
+                                        & AS.concat
+                      f2 <-
+                          S.toList
+                              $ S.bracket_ (openFile f ReadMode) hClose
+                              $ \h -> S.unfold H.read h
+                      f1 `shouldBe` f2
 
 main :: IO ()
 main = do
@@ -53,13 +92,16 @@ main = do
     let corpora = base ++ "/corpora/"
     hspec
         $ do
-            describe "QuickCheck"
+            describe "Identity"
                 $ do
-                    it "decompressResizedD . compressD == id"
-                        $ property $ identitySimple
-            describe "Corpora"
-                $ do
+                    it "decompressResized . compress == id"
+                        $ property $ decompressResizedcompressSimple
                     it
                         ("decompressResized . compress == id "
                              ++ cantrbry_alice29_txt "(" ++ ")")
-                        $ identityRealWorld 5 (cantrbry_alice29_txt corpora)
+                        $ decompressResizedcompress
+                        $ cantrbry_alice29_txt corpora
+                    it
+                        ("decompress . compress == id "
+                             ++ cantrbry_alice29_txt "(" ++ ")")
+                        $ decompressCompress $ cantrbry_alice29_txt corpora
