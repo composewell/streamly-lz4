3789ff9f93a3f61d60579acd5f01b47eba80967d
Adithya Kumar <adithya@composewell.com>
2020-12-16 15:28:25 +0530

    Add a simple identity test

--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,34 @@
 module Main (main) where
 
+import Data.Word (Word8)
+import Test.Hspec (describe, hspec, it)
+import Test.QuickCheck (Property, forAll, property)
+import Test.QuickCheck.Gen (Gen, choose, chooseAny, listOf)
+import Test.QuickCheck.Monadic (monadicIO)
+
+import qualified Streamly.Internal.Data.Array.Storable.Foreign.Types as A
+import qualified Streamly.Internal.Data.Stream.StreamD as D
+
+import Streamly.Compression.LZ4
+
+genArrayW8List :: Gen [A.Array Word8]
+genArrayW8List = listOf $ A.fromList <$> listOf chooseAny
+
+identitySimple :: Property
+identitySimple =
+    forAll ((,) <$> genArrayW8List <*> choose (-10, 10))
+        $ \(lst, i) ->
+              let strm = D.fromList lst
+               in monadicIO
+                      $ do
+                          lst1 <-
+                              D.toList $ decompressResizedD $ compressD i strm
+                          return $ lst == lst1
+
 main :: IO ()
-main = print "Run tests here"
+main =
+    hspec
+        $ describe "StreamD"
+        $ do
+            it "decompressResizedD . compressD == id"
+                $ property $ identitySimple
